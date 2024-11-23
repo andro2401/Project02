87ad706b2d3e083eee0f309fad81cd4eebc8095b
Andro <andro.istuk@elatus.net>
2024-11-23 13:57:46 +0100

    .

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {globalStyles} from '../styles/Global.tsx';
 
 const FlatCards = () => {
   return (
-    <ScrollView style={globalStyles.container}>
-      <Text style={globalStyles.title}>Flat Cards</Text>
-    </ScrollView>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Basic Cards</Text>
+      <View style={styles.basicCardWrapper}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text style={styles.cardText}>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text style={styles.cardText}>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text style={styles.cardText}>Green</Text>
+        </View>
+      </View>
+    </View>
   );
 };
 const styles = StyleSheet.create({
-
+  basicCardWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: 10,
+    flexWrap: 'wrap',
+  },
+  card:{
+    flex: 1,
+    borderRadius: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 50,
+  },
+  cardText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  cardOne:{
+    backgroundColor: '#BF3100',
+  },
+  cardTwo:{
+    backgroundColor: '#044389',
+  },
+  cardThree:{
+    backgroundColor: '#8EA604',
+  },
 });
 export default FlatCards;
