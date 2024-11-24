5a48e988db1cffff24bcfd390efe352933c50bc7
Andro <andro.istuk@elatus.net>
2024-11-24 11:56:11 +0100

    contact card started

---
+++ components/ContactList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const ContactList = () => {
+  return (
+    <View>
+      <Text>Contact List</Text>
+      <View></View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  card:{},
+  cardImg:{},
+  cardName:{},
+  cardMessage:{},
+});
+export default ContactList;
